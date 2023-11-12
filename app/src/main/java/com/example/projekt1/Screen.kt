54d9198ff3d066bec4f9a0fffd84f528c0a0a3ad
pochodyla <pochodyla@gmail.com>
2023-11-12 21:48:04 +0100

    done

--- app/src/main/java/com/example/projekt1/Screen.kt
+++ app/src/main/java/com/example/projekt1/Screen.kt
@@ -4,16 +4,4 @@ sealed class Screen(val route: String) {
     object MainScreen : Screen("main_screen")
     object SecondScreen : Screen("second_screen")
     object ThirdScreen : Screen("third_screen")
-
-
-    fun withArgs(vararg args: String) : String {
-        return buildString {
-            append(route)
-            args.forEach { arg ->
-                append("/$arg")
-            }
-        }
-    }
-
-
 }
--- app/src/main/java/com/example/projekt1/ui/theme/Navigation.kt
+++ app/src/main/java/com/example/projekt1/ui/theme/Navigation.kt
@@ -1,7 +1,10 @@
 package com.example.projekt1.ui.theme
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
@@ -9,12 +12,18 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.navigation.NavController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.projekt1.R
 import com.example.projekt1.Screen
 
 @Composable
@@ -27,7 +36,7 @@ fun Navigation() {
         composable(route = Screen.MainScreen.route) {
             MainScreen(navController = navController)
         }
-        composable(route = Screen.SecondScreen.route + "/{name}",
+        composable(route = Screen.SecondScreen.route + "?name={name}",
             arguments = listOf(
                 navArgument("name") {
                     type = NavType.StringType
@@ -51,9 +60,11 @@ fun MainScreen(navController: NavController) {
     }
     Column(
         verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 50.dp)
+            .background(Color.Yellow)
     ){
         TextField(value = text, onValueChange = {
             text = it
@@ -62,23 +73,34 @@ fun MainScreen(navController: NavController) {
             .fillMaxWidth())
         Spacer(modifier = Modifier.height(8.dp))
         Button(onClick = {
-                         navController.navigate(Screen.SecondScreen.route + "/$text")
+                         navController.navigate(Screen.SecondScreen.route + "$text")
         }, modifier = Modifier.align(Alignment.End)){
             Text(text = "do drugiego ekranu")
     }
+        Image(
+            painterResource(R.drawable.img),
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
 
 @Composable
 fun SecondScreen(name: String?, navController: NavController) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Green)
+        , contentAlignment = Alignment.Center){
         Text(text = "witam na drugim ekranie, $name")
     }
 Column(
     verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally,
     modifier = Modifier
         .fillMaxWidth()
-        .padding(horizontal = 50.dp)){}
+        .padding(horizontal = 50.dp)
+        .background(Color.Green)){}
     Button(onClick = {
         navController.navigate(Screen.ThirdScreen.route)
     }){
@@ -88,7 +110,11 @@ Column(
 
 @Composable
 fun ThirdScreen(navController: NavController) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.LightGray),
+        contentAlignment = Alignment.Center){
         Text(text = "to jest trzeci ekran")
     }
     Button(onClick = {
